a91adaddbe679de84c5fd5864cde09c1003d9c1e
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-05-28 19:58:05 -0700

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 120,
+  tabWidth: 4,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+}

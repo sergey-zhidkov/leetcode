b877e077db3fcd1ad4a88adefca411aa25e21f4e
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-07-01 00:06:47 -0700

    128

---
+++ neetcode-150/128-longest-consecutive-sequence.ts
@@ -0,0 +1,66 @@
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+// Example 2:
+
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+
+// Constraints:
+
+//     0 <= nums.length <= 105
+//     -109 <= nums[i] <= 109
+
+function longestConsecutive(nums: number[]): number {
+    if (!nums?.length) {
+        return 0
+    }
+
+    const numToVisitedMap = new Map<number, boolean>()
+    for (const singleNum of nums) {
+        numToVisitedMap.set(singleNum, false)
+    }
+
+    let currLongestConsecutive = 1
+    for (const [singleNum, isVisited] of numToVisitedMap) {
+        if (isVisited) {
+            continue
+        }
+        numToVisitedMap.set(singleNum, true)
+        const visited = visitNeighbours(singleNum, numToVisitedMap) + 1
+        if (visited > currLongestConsecutive) {
+            currLongestConsecutive = visited
+        }
+    }
+
+    function visitNeighbours(singleNum: number, map: Map<number, boolean>): number {
+        let visited = 0
+
+        // traverse left
+        let nextNum = singleNum - 1
+        while (map.has(nextNum)) {
+            map.set(nextNum, true)
+            visited++
+            nextNum--
+        }
+
+        // traverse right
+        nextNum = singleNum + 1
+        while (map.has(nextNum)) {
+            map.set(nextNum, true)
+            visited++
+            nextNum++
+        }
+
+        return visited
+    }
+
+    return currLongestConsecutive
+}

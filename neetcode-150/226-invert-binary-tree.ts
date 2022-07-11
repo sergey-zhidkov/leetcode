5185759ba718250c40e76e973008598a3ee72451
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-07-10 23:30:46 -0700

    226

---
+++ neetcode-150/226-invert-binary-tree.ts
@@ -0,0 +1,41 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val
+        this.left = left === undefined ? null : left
+        this.right = right === undefined ? null : right
+    }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) {
+        return null
+    }
+
+    const left = root.left
+    root.left = root.right
+    root.right = left
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+}

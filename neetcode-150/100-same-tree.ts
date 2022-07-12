26430ee411d8c484fe9aa797905d2b85b519a8a7
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-07-12 16:20:02 -0700

    100

---
+++ neetcode-150/100-same-tree.ts
@@ -0,0 +1,57 @@
+// Given the root of a binary tree, return the length of the diameter of the tree.
+
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+// The length of a path between two nodes is represented by the number of edges between them.
+
+// Example 1:
+
+// Input: root = [1,2,3,4,5]
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+// Example 2:
+
+// Input: root = [1,2]
+// Output: 1
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val
+        this.left = left === undefined ? null : left
+        this.right = right === undefined ? null : right
+    }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (!p && !q) {
+        return true
+    }
+
+    if ((p && !q) || (!p && q)) {
+        return false
+    }
+
+    if (p.val === q.val) {
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+
+    return false
+}

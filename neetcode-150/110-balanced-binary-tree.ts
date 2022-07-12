e6cd4b71dd809ac40d19f11980fc4c7adb4cae37
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-07-12 16:22:13 -0700

    update description

--- neetcode-150/110-balanced-binary-tree.ts
+++ neetcode-150/110-balanced-binary-tree.ts
@@ -1,33 +1,8 @@
-// Given the root of a binary tree, return the length of the diameter of the tree.
+// Given a binary tree, determine if it is height-balanced.
 
-// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+// For this problem, a height-balanced binary tree is defined as:
 
-// The length of a path between two nodes is represented by the number of edges between them.
-
-// Example 1:
-
-// Input: root = [1,2,3,4,5]
-// Output: 3
-// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
-
-// Example 2:
-
-// Input: root = [1,2]
-// Output: 1
-
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+//     a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
 
 class TreeNode {
     val: number

d645a782596094e2ca07233fed92ea14d7088700
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-07-15 02:26:11 -0700

    206

---
+++ neetcode-150/206-reverse-linked-list.ts
@@ -0,0 +1,45 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Example 2:
+
+// Input: head = [1,2]
+// Output: [2,1]
+
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val
+        this.next = next === undefined ? null : next
+    }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+    if (!head) {
+        return head
+    }
+
+    let prev: ListNode | null = null
+    let currentHead: ListNode | null = head
+    while (currentHead) {
+        const oldNext = currentHead.next
+        const newNext = prev
+        prev = currentHead
+
+        currentHead.next = newNext
+        currentHead = oldNext
+    }
+
+    return prev
+}
